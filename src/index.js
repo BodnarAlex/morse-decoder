46f6aa36bfe1def8f7bf57181815075dba155646
Bodnar Alex <allugovskova@mail.ru>
2023-08-20 15:06:48 +0200

    refactor: improve script

--- src/index.js
+++ src/index.js
@@ -41,9 +41,9 @@ function decode(expr) {
     let res = [];
     let count = expr.length / 10;
     for (let i = 0; i < count ; i++){
-        res = res.concat(expr.slice(10 * i, 10 * i + 10).replaceAll('10', '.').replaceAll('11', '-').replaceAll('0', '').replaceAll('*', ' '));
+        res = res.concat(expr.slice(10 * i, 10 * i + 10).replaceAll('10', '.').replaceAll('11', '-').replaceAll('0', ''));
     }
-    return res.map((x)=> (x == " ") ? x : MORSE_TABLE[x]).join('');
+    return res.map((x)=> (x.includes('*')) ? ' ' : MORSE_TABLE[x]).join('');
 }
 
 module.exports = {
